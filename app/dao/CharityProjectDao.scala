5a3ae196d8f2f7fe505a85145d1fbf3707647a58
Greg Dorrell <gredorrell@homeaway.com>
2019-03-15 16:26:46 +0000

    Add VolunteerDao, and allow volunteers and charities to be added

---
+++ app/dao/CharityProjectDao.scala
@@ -0,0 +1,43 @@
+package dao
+
+import model.CharityProject
+import play.api.libs.json.Json
+
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
+class CharityProjectDao {
+
+  private val charities: ListBuffer[CharityProject] = {
+//    val projectsRaw = Source.fromResource("charities.json").mkString
+//    val projectsJson = Json.parse(projectsRaw)
+//    projectsJson.validate[ListBuffer[CharityProject]].get
+    ListBuffer.empty
+  }
+
+  def getCharities(): List[CharityProject] = {
+    charities.toList
+  }
+
+  def add(charity: CharityProject): Unit = {
+    charities.append(charity)
+  }
+
+  def getSuggestedSkills(): List[String] = {
+    val allTags = for {
+      charity <- getCharities()
+      skill <- charity.skills
+    } yield skill
+
+    allTags.distinct
+  }
+
+  def getSuggestedCauses(): List[String] = {
+    val allCauses = for {
+      charity <- getCharities()
+      cause <- charity.causes
+    } yield cause
+
+    allCauses.distinct
+  }
+}
