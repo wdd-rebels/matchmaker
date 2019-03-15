5665644ebfd223853a5c7845f56088d85e6dc943
TessKi <kinnte@gmail.com>
2019-03-15 12:09:58 +0000

    started charity form

---
+++ public/pages/Charity-form.js
@@ -0,0 +1,42 @@
+<form>
+  <div className="form-group">
+    <label className="" htmlFor="charity">Charity name</label>
+    <input className="" type="text" name="charity" />
+  </div>
+
+  <div className="form-group">
+    <label className="" htmlFor="email">Email</label>
+    <input className="" type="email" name="email" />
+  </div>
+
+  <div className="form-group">
+    <label className="" htmlFor="cause">Cause</label>
+    <input className="" type="" name="cause" />
+  </div>
+
+  <div className="form-group">
+    <label className="" htmlFor="location">Location</label>
+    <input className="" type="" name="location" />
+  </div>
+
+  <div className="form-group">
+    <label className="" htmlFor="project">Project</label>
+    <input className="" type="" name="project" />
+  </div>
+
+  <div className="form-group">
+    <label className="" htmlFor="description">Description</label>
+    <textarea name="description" className="" type=""></textarea>
+  </div>
+
+  <div className="form-group">
+    <label className="" htmlFor="keywords">Keyword Tags</label>
+    <input className="" type="" name="keywords" />
+  </div>
+
+  <!-- <div className="form-group">
+    <label className="" htmlFor=""></label>
+    <input className="" type="" name="" />
+  </div> -->
+
+</form>
